5e5e394e8c788a552c291f6015c375247aaf0989
Pablo <pablito@me.com>
2025-04-18 00:25:48 +0100

    chore: first training model complete.

---
+++ data.js
@@ -0,0 +1,15 @@
+export const sampleData = {
+    currentLoss: null,
+    previousLoss: null,
+    weight: 0,
+    bias: 0,
+    examples: [
+        { feature: 3.5, label: 18 },
+        { feature: 3.69, label: 15 },
+        { feature: 3.44, label: 18 },
+        { feature: 3.43, label: 16 },
+        { feature: 4.34, label: 15 },
+        { feature: 4.42, label: 14 },
+        { feature: 2.37, label: 24 }
+    ]
+}
